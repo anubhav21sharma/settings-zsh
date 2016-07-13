b7e4d0c6f80b6041b52ad046a926c003f7ac1087
anubhav21sharma <anubhav21sharma@gmail.com>
2016-07-14 03:46:49 +0530

    Create install-findcycles.py

---
+++ install-findcycles.py
@@ -0,0 +1,7 @@
+ainstall pip
+ainstall python-dev
+sudo pip install networkx==1.9.1
+sudo pip install graphviz==0.4.4
+sudo pip install graphviz-dev
+sudo apt-get install libgraphviz-dev
+sudo pip install pygraphviz==1.2
